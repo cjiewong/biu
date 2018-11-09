ff90681450911915d0d3bf4f74227443401de97d
githcj <huangchaojie@baie.com.cn>
2018-11-09 15:13:25 +0800

    添加gradle对springboot打war包的支持

--- server/src/main/java/com/abc/ServerApplication.java
+++ server/src/main/java/com/abc/ServerApplication.java
@@ -2,14 +2,20 @@ package com.abc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * created by CaiBaoHong at 2018/4/17 14:15<br>
- *
+ * @author cjie
+ * 主类继承SpringBootServletInitializer，重写configure方法  可用gradle将springboot项目打包成war包 gradle文件中添加apply plugin: 'war'
  */
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class,args);
