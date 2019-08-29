f789d997ded7d7178d6ce65b425da3849b0a61b8
githcj <huangchaojie@baie.com.cn>
2019-08-29 13:50:09 +0800

    删除测试数据

--- server/src/main/java/com/makergo/dao/TestUserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.makergo.dao;
-
-import com.makergo.entity.TestUser;
-import com.baomidou.mybatisplus.mapper.BaseMapper;
-
-public interface TestUserMapper extends BaseMapper<TestUser> {
-}
